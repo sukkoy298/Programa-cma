2275c803cacf938c537dd6341976cd2346978c56
ant <65203561+antdestructor@users.noreply.github.com>
2020-05-19 10:39:21 -0400

    Update pageTaps.js

--- js/pageTaps.js
+++ js/pageTaps.js
@@ -1,9 +1,10 @@
 /* TAPS(Pestañas) */
-
+window.addEventListener('load', function () { 
+	$('#defaultOpen').click();
+ });
 function openPage(pageName,elmnt,color) {
-	
-var i, tabcontent, tablinks;
-tabcontent = document.getElementsByClassName("tabcontent");
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
 for (i = 0; i < tabcontent.length; i++) {
 	tabcontent[i].style.display = "none";
 }
@@ -11,11 +12,35 @@ tablinks = document.getElementsByClassName("tablink");
 for (i = 0; i < tablinks.length; i++) {
 	tablinks[i].style.backgroundColor = "";
 }
-document.getElementById(pageName).style.display = "block";
-elmnt.style.backgroundColor = color;
+	document.getElementById(pageName).style.display = "block";
+	elmnt.style.backgroundColor = color;
 }
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-/* TAPS(Pestañas) */
+var seleccionado = false;
+var datosPaciente = [];
+var contador = 0;
+var diferenteDe = false;
+function enviar() {		
+	while( document.getElementById('dtPaciente' + contador) != undefined){
+	datosPaciente[contador] = document.getElementById('dtPaciente' + contador).value;
+		if (datosPaciente[contador] != "") {
+			diferenteDe = true;
+		}else{
+			diferenteDe = false;
+		break;
+		}
+	contador ++;
+	}
+	if (diferenteDe == true){		
+		if(seleccionado == true){
+		}else{
+			openPage('Exams', document.getElementById('tabExam'), '#1E79E0');
+		}
+	}	
+}
+function check(checked) {
+	if (checked == true) {
+		seleccionado = true;		
+	}else{
+		seleccionado = false;
+	}
+}
