b3321b8e39cbfcf48e2e8e237481767bb8af6720
GabrielPerezZX <gabrieloctavioperez@gmail.com>
2020-05-25 20:52:15 -0400

    Estilos personalizado alos checkbox y correcciones

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Programa-cma
+
 Centro de Control de aplicaciones con multiples trabajadores, con el fin de optimizar las modificaciones y programacion del centro medico el angel, para mayor orden con los cambios que hagamos cada 1 de los miembros del equipo lleve un seguimiento de los archivos
--- js/pageTaps.js
+++ js/pageTaps.js
@@ -1,62 +1,62 @@
 /* TAPS(Pestañas) */
-window.addEventListener('load', function () { 
+window.addEventListener('load', function () {
 	$('#defaultOpen').click();
 	$.ajax({
-		type: "GET",
-		url: "php/Inicio_Caja.php",
+		type: 'GET',
+		url: 'php/Inicio_Caja.php',
 		success: function (response) {
 			$('#idfactura').attr('value', response);
-		}
+		},
 	});
 	var checkboxs = 1;
 	while (document.getElementById(checkboxs) != undefined) {
-	$('#' + checkboxs).attr('value', checkboxs);
-	checkboxs ++; 
+		$('#' + checkboxs).attr('value', checkboxs);
+		checkboxs++;
 	}
- });
-function openPage(pageName,elmnt,color) {
+});
+function openPage(pageName, elmnt, color) {
 	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-for (i = 0; i < tabcontent.length; i++) {
-	tabcontent[i].style.display = "none";
-}
-tablinks = document.getElementsByClassName("tablink");
-for (i = 0; i < tablinks.length; i++) {
-	tablinks[i].style.backgroundColor = "";
-}
-	document.getElementById(pageName).style.display = "block";
+	tabcontent = document.getElementsByClassName('tabcontent');
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none';
+	}
+	tablinks = document.getElementsByClassName('tablink');
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].style.backgroundColor = '';
+	}
+	document.getElementById(pageName).style.display = 'block';
 	elmnt.style.backgroundColor = color;
 }
-function enviar() {	
+function enviar() {
 	var checkboxs = 1;
 	var examenes = [];
 	var valor6;
 	var comporbanteVacios = false;
 	while (document.getElementById(checkboxs) != undefined) {
 		if (document.getElementById(checkboxs).checked == true) {
-	examenes[checkboxs] = $('#' + checkboxs).attr('value');
-	console.log (examenes[checkboxs]);
-	comporbanteVacios = true;
+			examenes[checkboxs] = $('#' + checkboxs).attr('value');
+			console.log(examenes[checkboxs]);
+			comporbanteVacios = true;
 		}
-		checkboxs ++; 
+		checkboxs++;
 	}
-	if (comporbanteVacios = false) {
-	valor6 = 'Consulta';
-	}else{
-	valor6 = 'Examenes';
+	if ((comporbanteVacios = false)) {
+		valor6 = 'Consulta';
+	} else {
+		valor6 = 'Examenes';
 	}
 	var valor1 = $('#fname').val();
 	var valor2 = $('#flastn').val();
 	var valor3 = $('#fcedula').val();
 	var valor4 = $('#fdateN').val();
 	var valor5 = $('#ftelf').val();
-	
+
 	$.ajax({
-		type: "POST",
-		url: "php/Caja.php",
-		data: { valor1, valor2, valor3, valor4, valor5, valor6,'examenes' : JSON.stringify(examenes)},
+		type: 'POST',
+		url: 'php/Caja.php',
+		data: { valor1, valor2, valor3, valor4, valor5, valor6, examenes: JSON.stringify(examenes) },
 		success: function (response) {
 			alert(response);
-		}
+		},
 	});
 }
