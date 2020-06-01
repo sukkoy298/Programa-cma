ed490dde83ef7927ee6f5ae5f5cceaf6b54d2184
ant <65203561+antdestructor@users.noreply.github.com>
2020-05-31 21:36:59 -0400

    Update inicio de seccion.php

--- php/inicio de seccion.php
+++ php/inicio de seccion.php
@@ -11,20 +11,22 @@ $direcciones;
     }else{
         if(!empty($usuario) && !empty($password)){
         $encuesta = "SELECT Status FROM users WHERE Usering = '$usuario' AND  Password = '$password'";
-        $fila = 'Status';
-        $area = query($encuesta, $fila);      
+        $campo = 'Status';
+        $area = query($encuesta, $campo);      
             if ($area == "nothing") {
             echo "<h2> Usuario o contraseña incorrectos </h2>";
             }else{
             $encuesta ="SELECT direcc FROM direcciones_archivos WHERE nivel = '$area'";
-            $fila = 'direcc';
-            $direcciones = query($encuesta, $fila);      
+            $campo = 'direcc';
+            $direcciones = query($encuesta, $campo);      
                 if ($direcciones) {   
                 header($direccion_carpeta . $direcciones);
                 }elseif(!$direcciones){
                     echo "<h2> Usuario o contraseña incorrectos </h2>";
                 }
             }
+        }else{
+        echo'que haces ';
         }
     }
 ?>
