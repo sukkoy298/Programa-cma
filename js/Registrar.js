8617dfdf20c65c7f6eabec7512f488c645989eda
GabrielPerezZX <gabrieloctavioperez@gmail.com>
2020-06-06 09:07:22 -0400

    Identado y formato de los archivos

--- js/Registrar.js
+++ js/Registrar.js
@@ -59,72 +59,72 @@
 			elementos[i].addEventListener('blur', blurInput);
 		}
 	}
-}());
+} () );
 var zona;
 var password;
-var password2; 
+var password2;
 var usuario1;
-window.addEventListener('load', function(){
-var div1 = document.getElementById('opciones');
-var div2;
-var div3;
-var a1;
-var img;
-var h2;
-var p1;
-var arreglo = ['admin','Administracion','Caja','Laboratorio'];
-    for (let index = 0; index < 4; index++) {
-        a1 = document.createElement('a');
-        $(a1).addClass('opcion');
-        div2 = document.createElement('div');
-        $(div2).addClass('contenido-opcion"');
-        div3 = document.createElement('div');
-        $(div3).addClass('textos');
-        h2 = document.createElement('h2');
-        $(h2).addClass('titulo');
-        h2.id = arreglo[index];
-        $(h2).attr('onclick','seleccionar(id)');
-        h2.innerHTML = arreglo[index];
-        p1 = document.createElement('p');
-        $(p1).addClass('descripcion');
-        div3.appendChild(h2);
-        div3.appendChild(p1);
-        div2.appendChild(div3);
-        a1.appendChild(div2);
-        div1.appendChild(a1);
-}
-    $('.opciones').hide();
+window.addEventListener('load', function () {
+	var div1 = document.getElementById('opciones');
+	var div2;
+	var div3;
+	var a1;
+	var img;
+	var h2;
+	var p1;
+	var arreglo = ['admin', 'Administracion', 'Caja', 'Laboratorio'];
+	for (let index = 0; index < 4; index++) {
+		a1 = document.createElement('a');
+		$(a1).addClass('opcion');
+		div2 = document.createElement('div');
+		$(div2).addClass('contenido-opcion"');
+		div3 = document.createElement('div');
+		$(div3).addClass('textos');
+		h2 = document.createElement('h2');
+		$(h2).addClass('titulo');
+		h2.id = arreglo[index];
+		$(h2).attr('onclick', 'seleccionar(id)');
+		h2.innerHTML = arreglo[index];
+		p1 = document.createElement('p');
+		$(p1).addClass('descripcion');
+		div3.appendChild(h2);
+		div3.appendChild(p1);
+		div2.appendChild(div3);
+		a1.appendChild(div2);
+		div1.appendChild(a1);
+	}
+	$('.opciones').hide();
 });
 var condicion = 1;
 function mostrar() {
-    if (condicion == 1) {
-        $('.opciones').show();
-        condicion = 0;
-    }else{
-        $('.opciones').hide();
-        condicion = 1;
-    }
+	if (condicion == 1) {
+		$('.opciones').show();
+		condicion = 0;
+	} else {
+		$('.opciones').hide();
+		condicion = 1;
+	}
 }
 function seleccionar(id) {
-    zona = id;
-   document.getElementById('titulo_seleccion').innerHTML = id;
-   mostrar(); 
+	zona = id;
+	document.getElementById('titulo_seleccion').innerHTML = id;
+	mostrar();
 }
 function registrar() {
-    usuario = $('#nombre').val();
-    password = $('#pass').val();
-    password2 = $('#pass2').val();
-        if (usuario1 != "" && password != "" && password2 != "" && zona != "") {
-            if (password == password2) {
-                $.ajax({
-                    type: "POST",
-                    url: "php/Registro_Usuario.php",
-                    data: { usuario1, password, zona },
-                    success: function (response) {
-                        alert('OLA');
-                     alert(response);   
-                    }
-                });
-            }
-        }   
+	usuario = $('#nombre').val();
+	password = $('#pass').val();
+	password2 = $('#pass2').val();
+	if (usuario1 != '' && password != '' && password2 != '' && zona != '') {
+		if (password == password2) {
+			$.ajax({
+				type: 'POST',
+				url: 'php/Registro_Usuario.php',
+				data: { usuario1, password, zona },
+				success: function (response) {
+					alert('OLA');
+					alert(response);
+				},
+			});
+		}
+	}
 }
