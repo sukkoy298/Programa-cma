4c4fe64cb0a13d0ee5c7179d0f3ffb750a7b5d39
GabrielPerezZX <gabrieloctavioperez@gmail.com>
2020-06-16 19:58:49 -0400

    Agregue alertas a cajaD

--- js/ValidarCd.js
+++ js/ValidarCd.js
@@ -3,14 +3,62 @@ var formulario = document.form_datosP,
 	elementos = formulario.elements;
 
 // Funcion que se ejecuta cuando el evento click es activado
-
 var validarInputs = function () {
 	for (var i = 1; i < elementos.length; i++) {
 		// Identificamos si el elemento es de tipo texto, email, password, radio o checkbox
 		if (elementos[i].type == 'text' || elementos[i].type == 'number' || elementos[i].type == 'date') {
 			// Si es tipo texto o password vamos a comprobar que esten completados los input
 			if (elementos[i].value.length == 0) {
-				console.log('El campo ' + elementos[i].name + ' esta incompleto');
+				// console.log('El campo ' + elementos[i].name + ' esta incompleto');
+				if (elementos[i].name == 'fname') {
+					Swal.fire({
+						title: 'El campo nombre esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				} else if (elementos[i].name == 'flastn') {
+					Swal.fire({
+						title: 'El campo apellido esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				} else if (elementos[i].name == 'fcedula') {
+					Swal.fire({
+						title: 'El campo cedula esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				} else if (elementos[i].name == 'fdateN') {
+					Swal.fire({
+						title: 'El campo fecha de nacimiento esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				}
 				elementos[i].className = elementos[i].className + ' error';
 				return false;
 			} else {
@@ -25,8 +73,19 @@ var enviar = function () {
 	if (!validarInputs()) {
 		console.log('Falto validar los Input');
 	} else {
-		console.log('Enviando');
+		// console.log('Enviando');
 		// Antoni coloca el codigo de para enviar los datos aqui xd
+		Swal.fire({
+			title: 'Enviando',
+			icon: 'success',
+			timer: '4000',
+			timerProgressBar: true,
+			toast: true,
+			position: 'top-end',
+			allowEscapeKey: false,
+			stopKeydownPropagation: false,
+			showConfirmButton: false,
+		});
 	}
 };
 
