153b7678ad64f702246addc12d7c78c51a7bd027
GabrielPerezZX <gabrieloctavioperez@gmail.com>
2020-06-16 20:09:45 -0400

    Agregue alertas al historial medico

--- js/ValidarHm.js
+++ js/ValidarHm.js
@@ -3,14 +3,62 @@ var formulario = document.form_buscar,
 	elementos = formulario.elements;
 
 // Funcion que se ejecuta cuando el evento click es activado
-
 var validarInputs = function () {
 	for (var i = 0; i < elementos.length; i++) {
 		// Identificamos si el elemento es de tipo texto, email, password, radio o checkbox
 		if (elementos[i].type == 'text' || elementos[i].type == 'number' || elementos[i].type == 'date') {
 			// Si es tipo texto, email o password vamos a comprobar que esten completados los input
 			if (elementos[i].value.length == 0) {
-				console.log('El campo ' + elementos[i].name + ' esta incompleto');
+				// console.log('El campo ' + elementos[i].name + ' esta incompleto');
+				if (elementos[i].name == 'numeroP') {
+					Swal.fire({
+						title: 'El campo numero de paciente esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				} else if (elementos[i].name == 'cedulaP') {
+					Swal.fire({
+						title: 'El campo cedula del paciente esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				} else if (elementos[i].name == 'dateNP') {
+					Swal.fire({
+						title: 'El campo fecha de nacimiento del paciente esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				} else if (elementos[i].name == 'nombreP') {
+					Swal.fire({
+						title: 'El campo nombre completo del paciente esta incompleto',
+						icon: 'error',
+						timer: '4000',
+						timerProgressBar: true,
+						toast: true,
+						position: 'top-end',
+						allowEscapeKey: false,
+						stopKeydownPropagation: false,
+						showConfirmButton: false,
+					});
+				}
 				elementos[i].className = elementos[i].className + ' error';
 				return false;
 			} else {
@@ -25,8 +73,19 @@ var enviar = function () {
 	if (!validarInputs()) {
 		console.log('Falto validar los Input');
 	} else {
-		console.log('Enviando');
+		// console.log('Enviando');
 		//Antoni coloca el codigo para enviar los datos aqui xd
+		Swal.fire({
+			title: 'Enviando',
+			icon: 'success',
+			timer: '4000',
+			timerProgressBar: true,
+			toast: true,
+			position: 'top-end',
+			allowEscapeKey: false,
+			stopKeydownPropagation: false,
+			showConfirmButton: false,
+		});
 	}
 };
 
