a83e26931d0f3a72f165eda536a726cc31bad525
ant <65203561+antdestructor@users.noreply.github.com>
2020-06-26 13:32:51 -0400

    Update Historial_Medico_ExaConsul.php

--- php/Historial_Medico_ExaConsul.php
+++ php/Historial_Medico_ExaConsul.php
@@ -2,12 +2,12 @@
 include('Conexion.php');
 try {
 $ID_paciente = $_POST['id'];
-$facturas = "SELECT ID_Num, Fecha FROM facturas WHERE ID_Pacient = '$ID_paciente'";
+$facturas = "SELECT ID, Fecha FROM Factura WHERE Paciente = '$ID_paciente'";
 $conetar = mysqli_query($conexion, $facturas);
 $ID_Factura = array();
 while ($resultado = mysqli_fetch_array($conetar)) {
 $ID_Factura [] = array(
-'ID' => $resultado['ID_Num'], 'Fecha' => $resultado['Fecha']
+'ID' => $resultado['ID'], 'Fecha' => $resultado['Fecha']
 );
 }
 $json = json_encode($ID_Factura);
