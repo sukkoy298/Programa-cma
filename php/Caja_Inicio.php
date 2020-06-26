20a2e696f2de9213418f144d1a15ad5658d0bc42
ant <65203561+antdestructor@users.noreply.github.com>
2020-06-26 13:31:27 -0400

    Update Caja_Inicio.php

--- php/Caja_Inicio.php
+++ php/Caja_Inicio.php
@@ -1,13 +1,16 @@
 <?php
 include ('Conexion.php');
-$encuesta = 'SELECT ID_Num FROM facturas';
-$conectar = mysqli_query($conexion,$encuesta);
-while ($resultado = mysqli_fetch_array($conectar)) {
-  $ID_Factura = $resultado['ID_Num']; 
-}
-if (is_numeric($ID_Factura)) {
-    echo $ID_Factura + 1;
-}else {
-  echo 1;
-}
+$encuesta = 'SELECT COUNT(ID) as total FROM factura';
+$cantidad = query($encuesta, 'total');
+$ultimo;
+if ($cantidad > 0 && is_numeric($cantidad)) {
+  $encuesta = 'SELECT ID FROM factura';
+  $conectar = mysqli_query($conexion,$encuesta);
+  while ($resultado = mysqli_fetch_array($conectar)) {
+    $ultimo = $resultado['ID']; 
+  }
+      echo $ultimo + 1;
+  }else {
+    echo 1; 
+  }
 ?>
